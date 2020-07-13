1900d35897caae16190b04a5f477487fa2a84e14
Matheus Reis <matheus.reis@olist.com>
2020-07-13 15:47:57 -0300

    Import problem solved

--- 02_AR_01/view/view_cmd.py
+++ 02_AR_01/view/view_cmd.py
@@ -1,3 +1,7 @@
+import sys
+import os.path
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from model.pessoa_fisica import PessoaFisica
 from dao.pessoa_fisica_dao import PessoaFisicaDao
 
