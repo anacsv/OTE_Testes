9ebe5d439904559c326fb11e41db88425ade74c7
Paulo Netto <pauloestrella1994@gmail.com>
2020-07-16 13:15:23 -0300

    Fixing update method

--- 02_AR_01/dao/pessoa_fisica_dao.py
+++ 02_AR_01/dao/pessoa_fisica_dao.py
@@ -28,11 +28,15 @@ class PessoaFisicaDao:
         return 'listar todos'
 
     def update(self, name_updated, pessoa_fisica:PessoaFisica):
-         #---- alterando a pessoa_fisica
+        #---- alterando a pessoa_fisica
+        # enum = 1
         with open('pessoa_fisica.txt', 'r+') as file:
             for nome in file:
+                file.readline()
                 if nome == pessoa_fisica.nome:
-                    file.write(pessoa_fisica.nome)
+                    file.seek(0, 0)
+                    file.write(name_updated)
+                    file.truncate()
                     return 'alterado'
                 else:
                     return 'This name doesn´t exist!'
@@ -40,3 +44,4 @@ class PessoaFisicaDao:
     def delete(self):
          #---- deletando a pessoa_fisica
         return 'excluído'
+
--- 02_AR_01/view/view_cmd.py
+++ 02_AR_01/view/view_cmd.py
@@ -7,4 +7,7 @@ from dao.pessoa_fisica_dao import PessoaFisicaDao
 
 pf = PessoaFisica(1,'maykon','05-11-86','44444444','55555555555')
 pfd = PessoaFisicaDao()
-print(pfd.create(pf))
+#print(pfd.create(pf))
+
+
+print(pfd.update('paulo', pf))
