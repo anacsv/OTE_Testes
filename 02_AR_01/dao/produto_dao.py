4ed9c2b1c8153f45f16eb2ab3d845508c7d937c7
Matheus Reis <matheus.reis@olist.com>
2020-07-20 11:28:45 -0300

    Solucao para model no construtor da base dao

--- 02_AR_01/dao/produto_dao.py
+++ 02_AR_01/dao/produto_dao.py
@@ -3,5 +3,8 @@ from dao.base_dao import BaseDao
 
 class ProdutoDao(BaseDao):
 
+    def __init__(self, classe):
+        super().__init__(classe)
+
     def create(self, produto: Produto):
         super().create(produto)
