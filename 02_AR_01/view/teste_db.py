42d4f5abf507ffb5bca8626987715aba787f17b6
Maykon D. Granemann <mgranemann@Mac-mini-de-Maykon.local>
2020-07-28 18:08:27 -0300

    CRUD db

--- 02_AR_01/view/teste_db.py
+++ 02_AR_01/view/teste_db.py
@@ -1,30 +1,16 @@
 import mysql.connector as connector
+import sys
+import os.path
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+)
 
-def list_all(cursor, command):
-    cursor.execute(sql_command)
-    all_users = cursor.fetchall()
-    for id, mail, password in all_users:
-        print(id ,mail ,password)
-
-def list_by_id(cursor, command):
-    cursor.execute(sql_command)
-    user = cursor.fetchone()
-    print(user[0] ,user[1] ,user[2])  
-
-def create(cursor, command, model):
-    pass
-
-def update(cursor, command, model):
-    pass
-
-def delete(cursor, command):
-    pass
+from model.usuario import Usuario
 
 hostname = 'mysql.padawans.dev' 
 username = 'padawans' 
 password = 'OTE2020' 
 database = 'padawans' 
-sql_command = 'SELECT id, mail, password from users'
 
 connection = connector.connect(
     host = hostname
@@ -32,10 +18,55 @@ connection = connector.connect(
     ,passwd = password
     ,db = database
 )
+def list_all(cursor, table_name):
+    sql_command = f'SELECT * from {table_name}'
+    cursor.execute(sql_command)
+    list_lines = cursor.fetchall()
+    return list_lines
+        
+
+def list_by_id(cursor, table_name, id):
+    sql_command = f'SELECT * from {table_name} where id = {id}'
+    cursor.execute(sql_command)
+    result = cursor.fetchone()
+    return result
+
+def create(cursor, sql_command):
+    cursor.execute(sql_command)
+    connection.commit()
+
+def update(cursor,sql_command):
+    cursor.execute(sql_command)
+    connection.commit()
+
+def delete(cursor, table_name, id):
+    sql_command = f'DELETE FROM {table_name} where id = {id};'
+    cursor.execute(sql_command)
+    connection.commit()
+
+u = Usuario('ana_delay@olist.com','delay_russia',123215)
+
+sql_command_insert = f'''INSERT INTO users
+                    VALUES
+                    (
+                        0
+                        ,'{u.email}'
+                        ,'{u.senha}'
+                    )
+                    ;'''
+
+sql_command_update = f'''UPDATE users 
+                    set
+                    mail = '{u.email}'
+                    ,password = '{u.senha}'
+                    where id = {u.id}; '''
 
 cursor = connection.cursor()
-create(cursor, sql_command)
-list_all(cursor, sql_command)
 
-
+# delete(cursor, 'users', 2)
+update(cursor, sql_command_update)
+result = list_all(cursor, 'users')
+print(result)
+# result = list_by_id(cursor, 'users',2)
+# print(result)
 connection.close()
